71970e19f3d2063666428b1d2ebef42d4970a3d0
ankit dixit <ankitdixit.nitb@gmail.com>
2018-11-30 17:29:46 +0530

    first commit

---
+++ src/main/java/in/akd/PpmFullStackApplication.java
@@ -0,0 +1,12 @@
+package in.akd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmFullStackApplication.class, args);
+	}
+}
