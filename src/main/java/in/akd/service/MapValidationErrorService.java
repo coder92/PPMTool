7edb8dcd481d96935db13a9d2fb63939b64f1d4f
ankit dixit <ankitdixit.nitb@gmail.com>
2018-12-02 14:29:27 +0530

    Service>>MapValidationErrorService

---
+++ src/main/java/in/akd/service/MapValidationErrorService.java
@@ -0,0 +1,26 @@
+package in.akd.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+@Service
+public class MapValidationErrorService {
+	
+	public ResponseEntity<?> mapValidationService(BindingResult result){
+		if(result.hasErrors()) {
+			Map<String, String> map = new HashMap<String,String>();
+			for(FieldError error:result.getFieldErrors()) {
+				map.put(error.getField(), error.getDefaultMessage());
+			}
+			return new ResponseEntity<Map<String,String>>(map, HttpStatus.BAD_REQUEST);
+		}
+		return null;
+	}
+
+}
--- src/main/java/in/akd/web/ProjectController.java
+++ src/main/java/in/akd/web/ProjectController.java
@@ -1,20 +1,18 @@
 package in.akd.web;
 
-import java.util.HashMap;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import in.akd.domain.Project;
+import in.akd.service.MapValidationErrorService;
 import in.akd.service.ProjectService;
 
 @RestController
@@ -23,21 +21,18 @@ public class ProjectController {
 
 	@Autowired
 	ProjectService projectService;
+	
+	@Autowired
+	MapValidationErrorService mapValidationErrorService;
 		
 	//@Valid - to get valid request body and response 
 	@PostMapping("") 
 	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
 		
-		HashMap<String, String> map = new HashMap<String,String>();
-		for(FieldError error:result.getFieldErrors()) {
-			map.put(error.getField(), error.getDefaultMessage());
+		ResponseEntity<?>errorMap = mapValidationErrorService.mapValidationService(result);
+		if(errorMap!=null) {
+			return errorMap;
 		}
-		
-		if(result.hasErrors()) {
-			// if object has errors then custom response
-			return new ResponseEntity<HashMap<String,String>>(map, HttpStatus.BAD_REQUEST);
-		}
-		
 		//save the project request object to db 
 		Project project1 = projectService.saveOrupdateProject(project);
 		
