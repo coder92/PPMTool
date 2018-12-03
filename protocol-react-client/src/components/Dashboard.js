ebef441121399c657db48cb8a6edbc1bf501f544
ankit dixit <ankitdixit.nitb@gmail.com>
2018-12-03 14:03:00 +0530

    created component folder and Dashboard.js

---
+++ protocol-react-client/src/components/Dashboard.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export default class Dashboard extends Component {
+  render() {
+    return (
+      <h1 className="m2-1">welcome to Dashboard</h1>
+    )
+  }
+}
+
